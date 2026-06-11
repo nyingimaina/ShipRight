bedc085a4a73deed02853dd8470071b923709da6
= <=>
2026-06-11 11:40:19 +0300

    chore: bump version 1.0.0 → 1.1.0

--- back-end/ShipRight/Program.cs
+++ back-end/ShipRight/Program.cs
@@ -105,7 +105,7 @@ try
 
     var projectCount = app.Services.GetRequiredService<IProjectStore>().Count;
     var buildCount   = app.Services.GetRequiredService<IBuildStore>().Count;
-    Log.Information("ShipRight {Version} starting on port {Port}", "1.0.0", 5200);
+    Log.Information("ShipRight {Version} starting on port {Port}", "1.1.0", 5200);
     Log.Information("Data directory: {DataDir}", dataDir);
     Log.Information("{ProjectCount} projects, {BuildCount} builds loaded", projectCount, buildCount);
 
--- shipright.iss
+++ shipright.iss
@@ -3,7 +3,7 @@
 ; Builds frontend + backend from source, then packages into setup.exe
 
 #define AppName "ShipRight"
-#define AppVersion "1.0.0"
+#define AppVersion "1.1.0"
 #define AppPublisher "ShipRight"
 #define OutputDir "installer"
 #define S SourcePath
